cfc10d749afcf6949338f7c14660e81e6f2b4528
Daniel Silveira <murtdaniel@gmail.com>
2024-03-19 17:19:59 -0300

    add new file Stack_LinkedList + peek on Stack

---
+++ Stack_LinkedList.js
@@ -0,0 +1,27 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek() { //O(1)
+        return this.top;
+    }
+
+    push(value) { // O(1)
+       
+    }
+
+    pop() { //O(1)
+
+    }
+
+}
