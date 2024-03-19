df6b72f1b1edb64f13b226da365ce8e6e72f2580
Daniel Silveira <murtdaniel@gmail.com>
2024-03-19 17:21:07 -0300

    add push on Stack

--- Stack_LinkedList.js
+++ Stack_LinkedList.js
@@ -17,7 +17,18 @@ class Stack {
     }
 
     push(value) { // O(1)
-       
+        const newNode = new Node(value);
+         
+        if (this.length === 0){
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
+
+        this.length++;
     }
 
     pop() { //O(1)
