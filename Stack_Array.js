4704bbb289c00971ea2d015b4d15c8489305b743
Daniel Silveira <murtdaniel@gmail.com>
2024-03-21 14:49:47 -0300

    add Stack_Array + peek method on Stack

---
+++ Stack_Array.js
@@ -0,0 +1,16 @@
+class Stack {
+    constructor() {
+        this.array = [];
+    }
+
+    peek() { //O(1)
+        return this.array[this.array.length - 1];
+    }
+
+    push(value) { // O(1)
+    }
+
+    pop() { //O(1)
+    }
+
+}
