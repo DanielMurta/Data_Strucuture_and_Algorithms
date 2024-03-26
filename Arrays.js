69a6dcf5b261bbd978be656ece1cb72455c6e3ee
Daniel Silveira <murtdaniel@gmail.com>
2024-03-26 16:58:40 -0300

    add Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,21 @@
+class MyArray {
+
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) { //O(1)
+    }
+
+    push(item) { //O(1)
+    }
+
+    pop() { //O(1)
+    }
+
+    delete(index) { //O(n)
+    }
+
+    
+}
